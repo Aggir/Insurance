bfed4976b032837ca35a6610c9e105a0b670d44c
Hussin Sherif <h.sherif@wassel.ly>
2023-07-18 12:23:35 +0200

    Fix: Status bar

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:insurance_app/presentation/theme/app_colors.dart';
 import '../app/constants.dart';
 import 'app_router.dart';
 import 'theme/app_theme.dart';
@@ -18,6 +19,10 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: AppColors.transparent,
+        statusBarBrightness: Brightness.dark,
+        statusBarIconBrightness: Brightness.dark));
     return MaterialApp.router(
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
